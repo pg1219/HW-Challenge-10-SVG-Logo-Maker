3289035e61d180c8038ba0a18c45e06eaad2bf0d
pg1219 <peter.gronseth@gmail.com>
2023-04-25 23:38:15 -0400

    initial push

---
+++ index.js
@@ -0,0 +1,46 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+// const generateMarkdown = require("./utils/generateMarkdown.js");
+
+// TODO: Create an array of questions for user input
+
+inquirer.prompt([
+    {
+    type: "input",
+    message: "Please enter up to 3 characters",
+    name: "characters",
+    validate: charNumber => {
+        if (charNumber.length <=3) {
+            return true;
+        } else { 
+            console.log(' Please enter 3 characters or less!');
+            return false; 
+        }
+    } 
+    },
+    {
+    type: "input",
+    message: "Please enter a color (or a hexadecimal number)",
+    name: "color",
+    },
+    {
+      type: "list",
+      message: "Please select a shape",
+      name: "shape",
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      type: "input",
+      message: "Please enter a shape color (or a hexadecimal number)",
+      name: "shapeColor",
+    } 
+  ])
+//   .then((data) => {
+//     console.log(data);
+    
+//     fs.writeFile("README.md", generateMarkdown(data), (error) => {
+//       error ? console.log(error) : console.log("successfully wrote file");
+//     });
+//   });
+  
